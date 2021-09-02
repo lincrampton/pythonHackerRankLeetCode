9e459dc122852e5c812922a5c32cb444af3aaf8e
lin crampton <lin.crampton@gmail.com>
2021-09-02 13:17:48 -0700

    Add files via upload

--- largestTimeFromDigits.py
+++ largestTimeFromDigits.py
@@ -1,27 +1,45 @@
-'''Given an array of 4 digits, return the largest 24 hour time that can be made.  The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a time is larger if more time has elapsed since midnight.  Return the answer as a string of length 5.  If no valid time can be made, return an empty string.'''
+'''Given an array arr of 4 digits, find the latest 24-hour time that can be made using each digit 
+exactly once. 24-hour times are formatted as "HH:MM", where HH is between 00 and 23, and MM is 
+between 00 and 59. The earliest 24-hour time is 00:00, and the latest is 23:59.
 
-class Solution1sept:
-    def largestTimeFromDigits(self, A): 
-        MAX_HOURS, MAX_MINUTES = 23, 59
-        A.sort()
-        for h in range(MAX_HOURS, -1, -1):
-            for m in range(MAX_MINUTES, -1, -1):
-                t = [h//10, h%10, m//10, m%10]
-                times = sorted(t)
-                if times == A:
-                    hours=10*t[0]+t[1]
-                    minutes=10*t[2]+t[3]
-                    return "{:02}:{:02}".format(hours, minutes)
-        return ""
-Sol = Solution1sept()
-print("17:11 answer is:", Sol.largestTimeFromDigits([1,1,1,7]))
-print("6:26 answer is:", Sol.largestTimeFromDigits([2,0,6,6]))
-print("4,30 answer is:", Sol.largestTimeFromDigits([0,0,3,4]))
-print("13:30 answer is:", Sol.largestTimeFromDigits([0,1,3,3]))
-print("23:51 answer is:", Sol.largestTimeFromDigits([2,1,5,3]))
-print("00:00 answer is:", Sol.largestTimeFromDigits([0,0,0,0]))
-print("09:09 answer is:", Sol.largestTimeFromDigits([9,9,0,0]))
-print("nada answer is:", Sol.largestTimeFromDigits([5,5,5,5]))
-print("19:06 answer is:", Sol.largestTimeFromDigits([1,9,0,6]))
-print("nada answer is:", Sol.largestTimeFromDigits([7,9,0,6]))
-print("nada answer is", Sol.largestTimeFromDigits([7,9,2,3]))
+Return the latest 24-hour time in "HH:MM" format.  If no valid time can be made, 
+return an empty string.'''
+
+# import itertools
+from itertools import permutations
+
+class Solution(object):
+    def largestTimeFromDigits(arr):
+        """
+        :type arr: List[int]
+        :rtype: str
+        """
+        result = ""
+        for i in range(len(arr)):
+            arr[i] *= 1
+        arr.sort(reverse=True)
+        
+        for h1, h2, m1, m2 in permutations(arr):
+            hours, mins = (10*h1 + h2), (10*m1 + m2)
+            if hours>=0 and hours<24 and mins>=0 and mins<60:
+                result = "{:02}:{:02}".format(hours, mins)
+                break
+        return result
+    
+    def largestTimeFromDigitsX(arr):
+        return max((f"{a}{b}:{c}{d}" for a,b,c,d in permutations(arr) 
+                   if f"{a}{b}" < "24" and f"{c}{d}" < "60"), default = "") 
+    
+    in1 = [0,0,1,0]
+    out1 = "10:00"
+    print(largestTimeFromDigits(in1), "should equal", out1, "and should equal", largestTimeFromDigitsX(in1))
+    
+    
+    in2 = [0,9,9,1]
+    out2 = "19:09"
+    print(largestTimeFromDigits(in2), "should equal", out2,"and should equal", largestTimeFromDigitsX(in2))
+    
+    in3 = [2,9,9,2]
+    out3 = ""
+    print(largestTimeFromDigits(in3), "should equal", out3,"and should equal", largestTimeFromDigitsX(in3))
+
